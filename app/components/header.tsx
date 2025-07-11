661e72e21f00855da69f1d0c50e294bfc58d85d2
GeoStormm <anass.bougazzoul2017@gmail.com>
2025-07-11 11:20:41 +0100

    Update images and assets in public folder

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -19,8 +19,8 @@ export default function Header({ searchQuery, onSearchChange }: HeaderProps) {
             <Image
               src="/voguevibe-logo.png"
               alt="VogueVibe Logo"
-              width={180}
-              height={40}
+              width={120}
+              height={32}
               priority
             />
           </div>
